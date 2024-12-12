ef58bf32498637db3e62fd802b70f3c8ee934e9c
Juande Megias Roca <82184200+miXTim@users.noreply.github.com>
2024-12-12 21:10:53 +0100

    Add files via upload

--- spreadsheet/app.js
+++ spreadsheet/app.js
@@ -1,29 +1,34 @@
 const container = document.querySelector("#dynamic");
 const operar = document.querySelector("#btnSum");
-const B9 = document.querySelector("#B9");  /* P(Ingr.2)=n1 */
+const A3 = document.querySelector("#A3");  /* P(Ingr.1) */
+const A4 = document.querySelector("#A4");  /* C(Ingr.1) */
+const A6 = document.querySelector("#A6");  /* P(Ingr.2)=n1 */
+const B6 = document.querySelector("#B6");
+const A7 = document.querySelector("#A7");
+const A9 = document.querySelector("#A9");
 const C9 = document.querySelector("#C9");
-const D9 = document.querySelector("#D9");
-const B10 = document.querySelector("#B10");
-const B13 = document.querySelector("#B13");
-const D13 = document.querySelector("#D13");
-const B14 = document.querySelector("#B14");  /* C(Ingr.2)=n2 */
-const C14 = document.querySelector("#C14");	 /* P(Ingr.3)=n3 */
-const B15 = document.querySelector("#B15");
-const E13 = document.querySelector("#E13");
+const A10 = document.querySelector("#A10");  /* C(Ingr.2)=n2 */
+const B10 = document.querySelector("#B10");	 /* P(Ingr.3)=n3 */
+const A11 = document.querySelector("#A11");
+const C11 = document.querySelector("#C11");
 
-function sum() {
+operar.addEventListener("click", function() {
   let n1 = Number(document.getElementById("n1").value);
   let n2 = Number(document.getElementById("n2").value);
   let n3 = Number(document.getElementById("n3").value);
-  let sum = Math.round((n2 * (100 - n1)) / n1);
-  B13.textContent = sum; // C(Ingr.1 + 3)
-  container.textContent = sum + " gr";
-  B9.textContent = n1; // P(Ingr.2)
-  B10.textContent = 100 - n1; // 100% - P(Ingr.2)
-  C9.textContent = n2; // C(Ingr.2)
-  B14.textContent = n2; // C(Ingr.2)
-  C14.textContent = n3; // P(Ingr.3)
-  B15.textContent = D13.textContent = n2 + sum; // C(total)
-  E13.textContent = ((n2 + sum) * 4) / 100 + " gr"; // C(Ingr.3)
-  E13.style.boxShadow = "0 0 2px 2px red inset";
-};
+  
+  if(n1 && n2 > 0) {
+	let sum = Math.round((n2 * (100 - n1)) / n1);
+	container.textContent = sum + " gr";
+	A3.textContent = 100 - (n1 + n3);  // P(Ingr.1)
+	A6.textContent = n1;  // P(Ingr.2)
+	A7.textContent = 100 - n1; // 100% - P(Ingr.2)
+	A9.textContent = sum;  // C(Ingr.1 + 3)
+	B6.textContent = A10.textContent = n2; // C(Ingr.2)
+	B10.textContent = n3; // P(Ingr.3)
+	A11.textContent = C9.textContent = n2 + sum; // C(total)
+	C11.textContent = ((n2 + sum) * n3) / 100 + " gr"; // C(Ingr.3)
+	C11.style.boxShadow = "0 0 2px 2px #308576 inset";
+	A4.textContent = (n2 + sum) - Math.round((n2 + ((n2 + sum) * n3) / 100)); // C(Ingr.1)
+  }
+})
